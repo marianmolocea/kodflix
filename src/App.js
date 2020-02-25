de6e0d3ede478d135aa0673c7456f6148f049a53
Marian Molocea <marian.mcrt@gmail.com>
2020-02-25 20:01:57 +0000

    Increase the font size of the title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Kodflix App!</p>
+        <h1>Welcome to Kodflix App!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
