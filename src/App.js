87bdb0ec694a601fea5e13cd81196c9ab87dc81c
Marian Molocea <marian.mcrt@gmail.com>
2020-02-25 19:56:14 +0000

    Title Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Kodflix!</p>
+        <p>Welcome to Kodflix App!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
