aa9a743bf3ddb8769917b07601e15301c98d4220
Marian Molocea <marian.mcrt@gmail.com>
2020-03-03 13:26:33 +0000

    Added the image overlay and the annimation

--- src/Covers.js
+++ src/Covers.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import arrow from './images/arrow.png';
 import blackList from './images/black-list.jpg';
-import got from './images/got.jpg'
-import prisonBreak from './images/prison-breack.jpg'
-import vikings from './images/vikings.jpg'
-import witcher from './images/witcher.jfif'
+import got from './images/got.jpg';
+import prisonBreak from './images/prison-breack.jpg';
+import vikings from './images/vikings.jpg';
+import witcher from './images/witcher.jfif';
 
-import Movie from './Movie'
+import Movie from './Movie';
 
 export default function Covers() {
   return (
-    <div className="container">
+    <div>
       <div className="row">
-        <Movie image={arrow} />
-        <Movie image={got} />
-        <Movie image={vikings} />
+        <Movie image={arrow} title="Arrow" />
+        <Movie image={got} title="Games of Thrones" />
+        <Movie image={vikings} title="The Vikings" />
       </div>
       <div className="row">
-        <Movie image={blackList} />
-        <Movie image={prisonBreak} />
-        <Movie image={witcher} />
+        <Movie image={blackList} title="Black List" />
+        <Movie image={prisonBreak} title="Prison Break" />
+        <Movie image={witcher} title="The Witcher" />
       </div>
     </div>
   );
--- src/Movie.js
+++ src/Movie.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Movie(props) {
   return (
-    <div>
-      <img src={props.image} alt="Movie image"/>
+    <div className="movie">
+      <img src={props.image} alt={props.title}/>
+      <div className="title">{props.title}</div>
     </div>
   )
 }
