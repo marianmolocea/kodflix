ef154e471e1d698ed95a7cd58fced21be58e8212
Marian Molocea <marian.mcrt@gmail.com>
2020-03-19 11:46:40 +0000

    create movie data for backend and install nodemon

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const movieData = require('./movieData.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(movieData));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
