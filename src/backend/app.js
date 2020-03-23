34f8517aadd253a4cb4e6bcbf443499e10f37488
Marian Molocea <marian.mcrt@gmail.com>
2020-03-23 14:37:38 +0000

    import path

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const movieData = require('./movieData.js');
+const path = require('path');
 
 app.get('/rest/shows', (req, res) => res.send(movieData));
 
