0823a0d601e03896ebfe16fbb79b23fd082c3c58
Marian Molocea <marian.mcrt@gmail.com>
2020-03-23 14:31:38 +0000

    changed paths

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,6 +7,7 @@ app.get('/rest/shows', (req, res) => res.send(movieData));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
