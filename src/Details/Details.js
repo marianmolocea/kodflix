f70473493eb68e8555f3355381e21a339efe1cdb
Marian Molocea <marian.mcrt@gmail.com>
2020-03-25 14:56:27 +0000

    New MovieDetails componet created

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import './Details.css'
+import MovieDetails from './MovieDetails/MovieDetails'
 
 export default class Details extends Component {
 
@@ -26,23 +27,10 @@ export default class Details extends Component {
     }
 
   render() {
-    if(!this.state.isLoaded){
-      return <div>Loading...</div>
-    } else if(!this.state.movie) {
-      return <Redirect to='/not-found' />
-    } else {
-      return (
-        <div className="Details">
-          <h2>
-            {this.state.movie.title}
-          </h2>
-          <div className="row">
-            <p>{this.state.movie.synopsis}</p>
-            <img src={require(`../images/${this.state.movie.image}`)} alt={this.state.movie.title}/>
-          </div>
-          <Link to="/"><button>Back to home page</button></Link>
-        </div>
-      );
-    }
+    return !this.state.isLoaded ? 
+      <div>Loading...</div> :
+      !this.state.movie ?
+        <Redirect to='/not-found' /> :
+        <MovieDetails movie={this.state.movie} />
   }
 }
