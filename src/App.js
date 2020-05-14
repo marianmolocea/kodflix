a89d1eb51187f2e9b6e437a6c32e4a7d2aac54e0
marianmolocea <marian.mcrt@gmail.com>
2020-05-14 21:25:36 +0100

    Add nav bar to all the pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App"> 
-        <Route exact path='/' component={NavBar} />
+        <Route path='/' component={NavBar} />
         <Route exact path='/' component={Gallery} />
         <Route exact path='/:movieId' render={(props) => <Details {...props} />} />
         <Route exact path='/not-found' component={NotFound} />
